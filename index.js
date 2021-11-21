6386d168e4d8d7861b859d9be2c79ed6024d68df
andrei-filip912 <filip.andrei912@gmail.com>
2021-11-21 17:39:51 +0100

    installed jwt-decode package, added fucntion for decoding

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cloudinary = require('./config/cloudinaryConfig');
 const {multerUploads, dataUri} = require('./middleware/multer');
 const mongodb = require('./config/mongodbConfig');
 const { jwtCheck } = require('./auth/checkJwt');
+const jwt_decode = require('jwt-decode');
 // const { checkJwt } = require('../client/src/api/auth0-express-js-sample-main/src/authz/check-jwt');
 // const apiRoute = express.Router();
 
@@ -22,7 +23,12 @@ app.use(json());
 
 // app.get('/*', (req, res) => res.sendFile(__dirname));
 // mongodb.getQuery();
+function getUserId(req) {
+	var rawToken = req.header('Authorization').split(" ");
+	var token = rawToken[1];
 
+	return jwt_decode(token);
+}
 
 app.post('/upload',[jwtCheck, multerUploads], function (req, res) {
 	if(req.file){
@@ -31,6 +37,10 @@ app.post('/upload',[jwtCheck, multerUploads], function (req, res) {
 
 		return cloudinary.uploader.upload(file).then((result) => {
 			const image = result.url;
+			
+			//jwt decoding
+			console.log(jwtDecoding(req));
+
 			return res.status(200).json({
 				messge: 'Your image has been uploded successfully to cloudinary',
 				data: {
