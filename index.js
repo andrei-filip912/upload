10d4bfa306b8b78a356adce2e70703fc29a427c3
andrei-filip912 <filip.andrei912@gmail.com>
2021-11-25 21:39:23 +0100

    Update index.js
    -added comment to index.js ,
    -added dbConnection string+method
    -start the app only after the connection is opened

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-require("dotenv").config();
+require("dotenv").config(); 
 const express = require("express");
 const cloudinary = require("./config/cloudinaryConfig");
 const { multerUploads, dataUri } = require("./middleware/multer");
 const { jwtCheck } = require("./auth/checkJwt");
 const jwt_decode = require("jwt-decode");
 const cors = require("cors");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
+// create the app, set the port
 const app = express();
 const port = process.env.PORT || 4000;
 
+// declare connection string and connect the app
+const dbURI = 'mongodb+srv://vscodeUser:' +
+  process.env.MONGODB_USER_PASSWORD +   
+  '@cluster0.ma8hc.mongodb.net/movie_upload?retryWrites=true&w=majority';
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(port, () => console.log(`Listening to port ${port}`)))
+  .catch(err => console.log(err));
+
+// using middleware cors, json for body parsing and urlencoded
 app.use(cors());
 app.use(express.json({ strict: false }));
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://vscodeUser:'+process.env.MONGODB_USER_PASSWORD+'@cluster0.ma8hc.mongodb.net/movie_upload?retryWrites=true&w=majority')
+
 
 app.get("/add-user_to_movie", (req, res) => {
-  
-});
-  
 
+});
 
 app.post("/upload", [jwtCheck, multerUploads], function (req, res) {
   console.log("start");
@@ -64,5 +71,3 @@ app.post("/upload", [jwtCheck, multerUploads], function (req, res) {
   }
   console.log("out");
 });
-
-app.listen(port, () => console.log(`Listening to port ${port}`));
